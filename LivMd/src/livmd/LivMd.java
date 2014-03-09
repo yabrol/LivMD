671c902eb79746656845a5bc314c7f0e943029fb
yabrol <yukti.abrol@gmail.com>
2014-03-08 23:01:35 -0500

    update & simplify

--- LivMd/src/livmd/LivMd.java
+++ LivMd/src/livmd/LivMd.java
@@ -1,159 +1,138 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package LivMd;
 
-package livmd;
+import LivMd.ui.LivMdUI;
+import LivMd.ui.LivMdErrorHandler;
+import xml_utilities.InvalidXMLFileFormatException;
+import properties_manager.PropertiesManager;
 
 /**
- *
- * @author Owner
+ * LivMd 
  */
-public class LivMd {
+public class LivMd
+{
+    // THIS HAS THE FULL USER INTERFACE AND ONCE IN EVENT
+    // HANDLING MODE, BASICALLY IT BECOMES THE FOCAL
+    // POINT, RUNNING THE UI AND EVERYTHING ELSE
+    static LivMdUI ui = new LivMdUI();
+    
+    // WE'LL LOAD ALL THE UI AND LANGUAGE PROPERTIES FROM FILES,
+    // BUT WE'LL NEED THESE VALUES TO START THE PROCESS
+    static String PROPERTY_TYPES_LIST = "property_types.txt";
+    static String UI_PROPERTIES_FILE_NAME = "properties.xml";
+    static String PROPERTIES_SCHEMA_FILE_NAME = "properties_schema.xsd";    
+    static String DATA_PATH = "./data/";
 
     /**
-     * @param args the command line arguments
+     * This is where the LivMd game application starts execution. We'll
+     * load the application properties and then use them to build our
+     * user interface and start the window in event handling mode. Once
+     * in that mode, all code execution will happen in response to a 
+     * user request.
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        try
+        {
+            // LOAD THE SETTINGS FOR STARTING THE APP
+            PropertiesManager props = PropertiesManager.getPropertiesManager();
+            props.addProperty(LivMdPropertyType.UI_PROPERTIES_FILE_NAME, UI_PROPERTIES_FILE_NAME);
+            props.addProperty(LivMdPropertyType.PROPERTIES_SCHEMA_FILE_NAME, PROPERTIES_SCHEMA_FILE_NAME);
+            props.addProperty(LivMdPropertyType.DATA_PATH.toString(), DATA_PATH);
+            props.loadProperties(UI_PROPERTIES_FILE_NAME, PROPERTIES_SCHEMA_FILE_NAME);
+                               
+            // NOW START THE UI IN EVENT HANDLING MODE
+            ui.startUI();            
+        }
+        // THERE WAS A PROBLEM LOADING THE PROPERTIES FILE
+        catch(InvalidXMLFileFormatException ixmlffe)
+        {
+            // LET THE ERROR HANDLER PROVIDE THE RESPONSE
+            LivMdErrorHandler errorHandler = ui.getErrorHandler();
+            errorHandler.processError(LivMdPropertyType.INVALID_XML_FILE_ERROR_TEXT);
+        }
     }
     
-    
     /**
-     * zombiecrushsagaPropertyType represents the types of data that will need
-     * to be extracted from XML files.
+     * LivMdPropertyType represents the types of data that will need
+ to be extracted from XML files. Using XML properties files
+     * makes it easy to switch between languages, which is important
+     * if one wants to maximize the number of users for an application.
      */
     public enum LivMdPropertyType
     {
         /* SETUP FILE NAMES */
         UI_PROPERTIES_FILE_NAME,
         PROPERTIES_SCHEMA_FILE_NAME,
-        GAME_FLAVOR_FILE_NAME,
-        RECORD_FILE_NAME,
 
         /* DIRECTORIES FOR FILE LOADING */
-        AUDIO_PATH,
         DATA_PATH,
         IMG_PATH,
         
-        /* WINDOW DIMENSIONS & FRAME RATE */
+        /* WINDOW DIMENSIONS */
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
-        FPS,
-        GAME_WIDTH,
-        GAME_HEIGHT,
-        GAME_LEFT_OFFSET,
-        GAME_TOP_OFFSET,
+        
+        /* LANGUAGE OPTIONS PROPERTIES */
+        LANGUAGE_OPTIONS,
+        LANGUAGE_DATA_FILE_NAMES,
+        LANGUAGE_IMAGE_NAMES,
         
         /* GAME TEXT */
+        SPLASH_SCREEN_TITLE_TEXT,
         GAME_TITLE_TEXT,
+        GAME_SUBHEADER_TEXT,
+        WIN_DISPLAY_TEXT,
+        GAME_RESULTS_TEXT,
+        GUESS_LABEL,
+        LETTER_OPTIONS,
         EXIT_REQUEST_TEXT,
-        INVALID_XML_FILE_ERROR_TEXT,
-        ERROR_DIALOG_TITLE_TEXT,
-        
-        /* ERROR TYPES */
-        AUDIO_FILE_ERROR,
-        LOAD_LEVEL_ERROR,
-        RECORD_SAVE_ERROR,
-        LOAD_LEVEL_REQS_ERROR,
+        YES_TEXT,
+        NO_TEXT,
+        DEFAULT_YES_TEXT,
+        DEFAULT_NO_TEXT,
+        DEFAULT_EXIT_TEXT,
 
         /* IMAGE FILE NAMES */
         WINDOW_ICON,
         SPLASH_SCREEN_IMAGE_NAME,
-        GAME_BACKGROUND_IMAGE_NAME,
-        SAGA_SCREEN_1_IMAGE_NAME,
-        SAGA_SCREEN_2_IMAGE_NAME,
-        SAGA_SCREEN_3_IMAGE_NAME,
-        SAGA_SCREEN_4_IMAGE_NAME,
-        SAGA_SCREEN_5_IMAGE_NAME,
-        SAGA_SCREEN_6_IMAGE_NAME,
-        SAGA_SCREEN_7_IMAGE_NAME,
-        SAGA_SCREEN_8_IMAGE_NAME,
-        SAGA_SCREEN_9_IMAGE_NAME,
+        GAME_IMG_NAME,
+        STATS_IMG_NAME,
+        HELP_IMG_NAME,
+        EXIT_IMG_NAME,
+        NEW_GAME_IMG_NAME,
+        HOME_IMG_NAME,
         
-        ABOUT_SCREEN_IMAGE_NAME,
-        LEVEL_SCREEN_IMAGE_NAME,
+        /* DATA FILE STUFF */
+        GAME_FILE_NAME,
+        STATS_FILE_NAME,
+        HELP_FILE_NAME,
+        WORD_LIST_FILE_NAME,
         
-        ABOUT_DIALOG_IMAGE_NAME,
-        LEVEL_DIALOG_IMAGE_NAME,
+        /* TOOLTIPS */
+        GAME_TOOLTIP,
+        STATS_TOOLTIP,
+        HELP_TOOLTIP,
+        EXIT_TOOLTIP,
+        NEW_GAME_TOOLTIP,
+        HOME_TOOLTIP,
         
-        BLANK_TILE_IMAGE_NAME,
-        BLANK_TILE_SELECTED_IMAGE_NAME,
+        /* FONT DATA */
+        LETTERS_FONT_FAMILY,
+        LETTERS_FONT_SIZE,
+        GUESSES_FONT_FAMILY,
+        GUESSES_FONT_SIZE,        
         
-        ZOMBIE_HAMMER_IMAGE_NAME,
-        
-        BACK_BUTTON_IMAGE_NAME,
-        BACK_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        TIME_IMAGE_NAME,
-        LIVES_IMAGE_NAME,
-        MOVES_IMAGE_NAME,
-        NEXT_STAR_IMAGE_NAME,
-        SCORE_IMAGE_NAME,
-//        STAR_IMAGE_NAME,
-        STAR_RED_IMAGE_NAME,
-        STAR_BLUE_IMAGE_NAME,
-        STAR_PURPLE_IMAGE_NAME,
-        STAR_WIN_RED_IMAGE_NAME,
-        STAR_WIN_BLUE_IMAGE_NAME,
-        STAR_WIN_PURPLE_IMAGE_NAME,
-        TILE_STACK_IMAGE_NAME,
-        
-        PLAY_BUTTON_IMAGE_NAME,
-        RESET_BUTTON_IMAGE_NAME,
-        QUIT_BUTTON_SPLASH_IMAGE_NAME,
-        PLAY_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        RESET_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        QUIT_BUTTON_SPLASH_MOUSE_OVER_IMAGE_NAME,
-        
-        QUIT_BUTTON_SAGA_IMAGE_NAME,
-        UP_BUTTON_IMAGE_NAME,
-        DOWN_BUTTON_IMAGE_NAME,
-        ABOUT_BUTTON_IMAGE_NAME,
-        QUIT_BUTTON_SAGA_MOUSE_OVER_IMAGE_NAME,
-        UP_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        DOWN_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        ABOUT_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        
-        RETURN_FROM_ABOUT_BUTTON_IMAGE_NAME,
-        RETURN_FROM_ABOUT_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        
-        PLAY_LEVEL_BUTTON_IMAGE_NAME,
-        PLAY_LEVEL_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        RETURN_FROM_LEVEL_BUTTON_IMAGE_NAME,
-        RETURN_FROM_LEVEL_BUTTON_MOUSE_OVER_IMAGE_NAME,
-        
-        // AND THE end game
-        WIN_IMAGE_NAME,
-        LOSS_IMAGE_NAME,
-        
-        /* TILE LOADING STUFF */
-        LEVEL_OPTIONS,
-        LEVEL_IMAGE_OPTIONS,
-        LEVEL_MOUSE_OVER_IMAGE_OPTIONS,
-        TYPE_A_TILES,
-        TYPE_B_TILES,
-        TYPE_C_TILES,
-        TYPE_D_TILES,
-        TYPE_E_TILES,
-        TYPE_F_TILES,
-        TYPE_STRIPED_TILES,
-        TYPE_BOMB_TILES,
-        TYPE_WRAPPER_TILES,
-        
-        LEVEL_REQUIREMENTS,
-        
-        /* AUDIO CUES */
-        SELECT_AUDIO_CUE,
-        MATCH_AUDIO_CUE,
-        NO_MATCH_AUDIO_CUE,
-        BLOCKED_TILE_AUDIO_CUE,
-        WIN_AUDIO_CUE,
-        LOSS_AUDIO_CUE,
-        SPLASH_SCREEN_SONG_CUE,
-        SAGA_SCREEN_SONG_CUE,
-        ABOUT_SCREEN_SONG_CUE,
-        LEVEL_SCREEN_SONG_CUE,
-        GAMEPLAY_SONG_CUE
+        /* THESE ARE FOR LANGUAGE-DEPENDENT ERROR HANDLING,
+           LIKE FOR TEXT PUT INTO DIALOG BOXES TO NOTIFY
+           THE USER WHEN AN ERROR HAS OCCURED */
+        ERROR_DIALOG_TITLE_TEXT,
+        DUPLICATE_WORD_ERROR_TEXT,
+        INVALID_WORD_ERROR_TEXT,
+        WRONG_WORD_ERROR_TEXT,
+        IMAGE_LOADING_ERROR_TEXT,
+        INVALID_URL_ERROR_TEXT,
+        INVALID_DOC_ERROR_TEXT,
+        INVALID_XML_FILE_ERROR_TEXT,
+        INVALID_DICTIONARY_ERROR_TEXT
     }
 }
